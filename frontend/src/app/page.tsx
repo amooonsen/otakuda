1018acc6e366a82bf7770ff5e3ed316541dd871f
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-04-15 12:52:49 +0900

    디렉토리 구조 분류 중

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,32 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
 export default function Home() {
   return (
-    <Button>버튼임</Button>
+    <>
+      <Button>버튼임</Button>
+      <Dialog>
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your account
+              and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+
+    </>
   );
 }
