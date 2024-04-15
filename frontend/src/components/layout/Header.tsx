35716ee18e6454f9418f49cd843ed8947091ea8e
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-04-15 15:16:49 +0900

    로그인 페이지 추가, 링크 연결

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -5,27 +5,36 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header id="header">
-      <div className="flex justify-between">
-      <Image
+      <div className="flex justify-between items-center">
+        <Link href='/'>
+          <span>Otakdua</span>
+          {/* <Image
         src='/svg/vercel.svg'
         alt="로고"
         width={200} // Image 컴포넌트에 width와 height 제공
         height={100}
-      />
-      <nav>
-        <ul className="flex gap-6">
-          <li>
-            <Link href='/'>Main</Link>
-          </li>
-          <li>
-            <Link href='/animation'>Anime</Link>
-          </li>
-          <li>
-            <Link href='/manga'>Manga</Link>
-          </li>
-        </ul>
-      </nav>
-      <ModeToggle />
+         /> */}
+      </Link>
+        <nav>
+          <ul className="flex gap-6">
+            <li>
+              <Link href='/'>Main</Link>
+            </li>
+            <li>
+              <Link href='/animation'>Anime</Link>
+            </li>
+            <li>
+              <Link href='/manga'>Manga</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex items-center gap-4">
+          <button>찾기</button>
+          <Link href='/login'>로그인/가입</Link>
+
+          <ModeToggle />
+
+        </div>
       </div>
     </header>
   );
